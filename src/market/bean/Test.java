8f8a0997948d6202150d07754ad605888b7af000
Entin <sanya_entin@mail.ru>
2017-01-17 11:45:06 +0200

    sintax

--- src/market/bean/Test.java
+++ src/market/bean/Test.java
@@ -4,12 +4,8 @@ package market.bean;
  * Created by anton on 17.01.17.
  */
 public class Test {
-    public static void main(String[] args) {
-
-
-
-    String kukusiki = "kukusiki";
-
-        System.out.println("Privet Okun!");
-}
+	public static void main(String[] args) {
+		String kukusiki = "kukusiki";
+		System.out.println("Privet Okun!");
+	}
 }
