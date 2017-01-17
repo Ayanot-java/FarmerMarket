738e0501f48cc2af53c3a1589b8d17d0e806e88d
genya_glushchenko <genroe@gmail.com>
2017-01-17 15:19:52 +0200

    product hibernate support added

--- src/market/bean/Product.java
+++ src/market/bean/Product.java
@@ -4,4 +4,64 @@ package market.bean;
  * Created by Tane4ka on 17.01.2017.
  */
 public class Product {
+    private int id;
+    private String name;
+    private String type;
+    private String img;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (id != product.id) return false;
+        if (name != null ? !name.equals(product.name) : product.name != null) return false;
+        if (type != null ? !type.equals(product.type) : product.type != null) return false;
+        if (img != null ? !img.equals(product.img) : product.img != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        result = 31 * result + (img != null ? img.hashCode() : 0);
+        return result;
+    }
 }
