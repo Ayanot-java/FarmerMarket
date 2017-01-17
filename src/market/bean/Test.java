0861107edd326c789a8224882b21ca2f55e8f47f
elena-malinovsky <Myllener@yandex.ru>
2017-01-17 11:57:22 +0200

    test push

--- src/market/bean/Test.java
+++ src/market/bean/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	public static void main(String[] args) {
 		String kukusiki = "kukusiki";
 		System.out.println("Privet Okun!");
-        System.out.println("raz-raz-raz");
+        System.out.println("raz-raz");
 
     }
 }
