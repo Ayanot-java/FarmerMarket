00a0b9f58db5527e58b7b0d9664e386509a6588c
Valentyn Shapiro <Lazyelectro@gmail.com>
2017-01-22 11:43:12 +0200

    StockDao added

---
+++ src/market/dao/StockDAOImpl.java
@@ -0,0 +1,28 @@
+package market.dao;
+
+import market.bean.Stock;
+
+/**
+ * Created by LazeL on 22.01.2017.
+ */
+public class StockDAOImpl implements StockDAO {
+    @Override
+    public void create(Stock stock) {
+
+    }
+
+    @Override
+    public Stock read(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void update(Integer id, Float availableQNT) {
+
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
+}
