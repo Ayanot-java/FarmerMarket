8c0d09b492d599adc8d29ef1942352f46aa4addb
Valentyn Shapiro <Lazyelectro@gmail.com>
2017-01-22 10:32:27 +0200

    Stock class added

---
+++ src/market/bean/Stock.java
@@ -0,0 +1,18 @@
+package market.bean;
+
+
+
+import javax.persistence.*;
+
+
+/**
+ * Created by LazeL (Valentyn Shapiro) on 22.01.2017.
+ */
+@Entity
+@Table(name = "stock")
+public class Stock {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Integer stockId;
+}
