cdb284474450e94504d158ca69f56810545bc283
Valentyn Shapiro <Lazyelectro@gmail.com>
2017-01-22 12:48:31 +0200

    StockDao methods implementation

--- src/market/dao/StockDAOImpl.java
+++ src/market/dao/StockDAOImpl.java
@@ -2,13 +2,36 @@ package market.dao;
 
 import market.bean.Stock;
 import market.bean.SupplyDetails;
+import market.util.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import javax.management.Query;
 
 /**
  * Created by LazeL on 22.01.2017.
  */
 public class StockDAOImpl implements StockDAO {
+    private static SessionFactory factory;
+
+    public StockDAOImpl(){
+        super();
+        factory = HibernateUtil.getSessionFactory();
+    }
+
+
     @Override
     public void create(SupplyDetails supplyDetails) {
+        Transaction tx = null;
+        try(Session session = factory.getCurrentSession();)
+        {
+            tx = session.beginTransaction();
+            Query query = session.createQuery("");
+        }catch (HibernateException e){
+            System.out.println(e.getStackTrace());
+        }
 
     }
 
