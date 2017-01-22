f960782fb57993fdecb455b3edb61997ff473e26
Valentyn Shapiro <Lazyelectro@gmail.com>
2017-01-22 16:28:08 +0200

    StockDao methods implementation

--- src/market/dao/StockDAOImpl.java
+++ src/market/dao/StockDAOImpl.java
@@ -3,8 +3,9 @@ package market.dao;
 import market.bean.Stock;
 import market.bean.SupplyDetails;
 import market.util.HibernateUtil;
-import org.hibernate.HibernateException;
+import org.hibernate.*;
 import org.hibernate.Session;
+import org.hibernate.query.*;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
@@ -16,7 +17,7 @@ import javax.management.Query;
 public class StockDAOImpl implements StockDAO {
     private static SessionFactory factory;
 
-    public StockDAOImpl(){
+    public StockDAOImpl() {
         super();
         factory = HibernateUtil.getSessionFactory();
     }
@@ -25,28 +26,69 @@ public class StockDAOImpl implements StockDAO {
     @Override
     public void create(SupplyDetails supplyDetails) {
         Transaction tx = null;
-        try(Session session = factory.getCurrentSession();)
-        {
+        try (Session session = factory.getCurrentSession();) {
             tx = session.beginTransaction();
-            Query query = session.createQuery("");
-        }catch (HibernateException e){
-            System.out.println(e.getStackTrace());
+//            Query query = (Query) session.createQuery("");
+            tx.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            if (tx != null) {
+                tx.rollback();
+
+            }
         }
 
     }
 
     @Override
     public Stock read(Integer id) {
-        return null;
+
+        Transaction tx = null;
+        Stock readStock = null;
+        try (Session session = factory.getCurrentSession()) {
+            tx = session.beginTransaction();
+            readStock = (Stock) session.get(Stock.class, id);
+            tx.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
+        return readStock;
     }
 
     @Override
     public void update(Integer id, Float newAvailableQnt) {
 
+        Transaction tx = null;
+        try(Session session = factory.getCurrentSession()){
+            tx = session.beginTransaction();
+            Stock updatableStock = (Stock) session.load(Stock.class, id);
+            Float qnt = updatableStock.getAvailableQnt();
+            qnt = newAvailableQnt;
+            session.update(updatableStock);
+            tx.commit();
+        }catch (HibernateException e){
+            e.printStackTrace();
+            if (tx!=null){
+                tx.rollback();
+            }
+        }
+
     }
 
     @Override
     public void delete(Integer id) {
+        Transaction tx = null;
+        try(Session session = factory.getCurrentSession()){
+            tx = session.beginTransaction();
+            Stock stockToDelete = (Stock) session.load(Stock.class, id);
+            session.delete(stockToDelete);
+            tx.commit();
+        }catch (HibernateException e){
+            e.printStackTrace();
+            if (tx!=null){
+                tx.rollback();
+            }
+        }
 
     }
 }
