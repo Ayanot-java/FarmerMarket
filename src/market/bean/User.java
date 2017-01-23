e92673ae44e07ec416651259f5e73ea27fcd0b4a
Vitaly Sokolov <sokoloff06@gmail.com>
2017-01-23 11:57:36 +0200

    User changes

--- src/market/bean/User.java
+++ src/market/bean/User.java
@@ -4,47 +4,43 @@ package market.bean;
 import javax.persistence.*;
 
 
-import javax.persistence.*;
-
-
-
 @Entity
 @Table(name = "user")
 public class User {
 
-	public User(String userName, String  passwd, String type) {
-		super();
-		this.userName = userName;
-		this. passwd =  passwd;
-		this.type = type;
-	}
-	
-	public User(String userName, String passwd){
-		this.userName = userName;
-		this.passwd =  passwd;
-	}
 	@Id
 	@GeneratedValue
 	@Column(name = "id")
 	private int id;
 
-	
 	@Column(name = "userName")
+	private String username;
 
-
-	private String userName;
-	
 	@Column(name = "passwd")
 	private String  passwd;
+
 	@Column(name = "type")
 	private String type;
 
-	public String getLoginName() {
-		return userName;
+	public User(String username, String  passwd, String type) {
+		super();
+		this.username = username;
+		this. passwd =  passwd;
+		this.type = type;
+	}
+	
+	public User(String username, String passwd){
+		this.username = username;
+		this.passwd =  passwd;
 	}
 
-	public void setLoginName(String loginName) {
-		this.userName = loginName;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String loginName) {
+		this.username = loginName;
 	}
 
 	public String getPassword() {
