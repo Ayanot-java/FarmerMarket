9d7c2e2b4c05cbc1e31fad97c6ccd1e7cdc5299c
Valentyn Shapiro <Lazyelectro@gmail.com>
2017-01-23 16:39:06 +0200

    Stock, StockDAO & StockDAOImpl fixed

--- src/market/bean/Stock.java
+++ src/market/bean/Stock.java
@@ -19,12 +19,12 @@ public class Stock {
 
     @ManyToMany
     @Column(name = "supply_details_id")
-    private SupplyDetails supplyDetails;
+    private Integer supplyDetailsId;
 
 
     @ManyToMany
     @Column(name = ("farmer_id"))
-    private Farmer farmer;
+    private Integer farmerId;
 
     @ManyToMany
     @Column(name = ("available_qnt"))
@@ -38,12 +38,34 @@ public class Stock {
     @Column(name = ("sPrice"))
     private Float sPrice;
 
+
+
     public Integer getStockId() {
+
         return stockId;
     }
 
-    public Float getAvailableQnt() {
+    public void setStockId(Integer stockId) {
+        this.stockId = stockId;
+    }
 
+    public Integer getSupplyDetailsId() {
+        return supplyDetailsId;
+    }
+
+    public void setSupplyDetailsId(Integer supplyDetailsId) {
+        this.supplyDetailsId = supplyDetailsId;
+    }
+
+    public Integer getFarmerId() {
+        return farmerId;
+    }
+
+    public void setFarmerId(Integer farmerId) {
+        this.farmerId = farmerId;
+    }
+
+    public Float getAvailableQnt() {
         return availableQnt;
     }
 
@@ -66,20 +88,6 @@ public class Stock {
     public void setsPrice(Float sPrice) {
         this.sPrice = sPrice;
     }
-
-    public SupplyDetails getSupplyDetails() {
-        return supplyDetails;
-    }
-
-    public Farmer getFarmer() {
-        return farmer;
-    }
-
-    public void setFarmer(Farmer farmer) {
-        this.farmer = farmer;
-
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -88,20 +96,23 @@ public class Stock {
         Stock stock = (Stock) o;
 
         if (getStockId() != null ? !getStockId().equals(stock.getStockId()) : stock.getStockId() != null) return false;
-        if (getSupplyDetails() != null ? !getSupplyDetails().equals(stock.getSupplyDetails()) : stock.getSupplyDetails() != null)
+        if (getSupplyDetailsId() != null ? !getSupplyDetailsId().equals(stock.getSupplyDetailsId()) : stock.getSupplyDetailsId() != null)
             return false;
-        if (getFarmer() != null ? !getFarmer().equals(stock.getFarmer()) : stock.getFarmer() != null) return false;
+        if (getFarmerId() != null ? !getFarmerId().equals(stock.getFarmerId()) : stock.getFarmerId() != null)
+            return false;
         if (getAvailableQnt() != null ? !getAvailableQnt().equals(stock.getAvailableQnt()) : stock.getAvailableQnt() != null)
             return false;
         if (getsDate() != null ? !getsDate().equals(stock.getsDate()) : stock.getsDate() != null) return false;
-        return getsPrice() != null ? getsPrice().equals(stock.getsPrice()) : stock.getsPrice() == null;
+        if (getsPrice() != null ? !getsPrice().equals(stock.getsPrice()) : stock.getsPrice() != null) return false;
+
+        return true;
     }
 
     @Override
     public int hashCode() {
         int result = getStockId() != null ? getStockId().hashCode() : 0;
-        result = 31 * result + (getSupplyDetails() != null ? getSupplyDetails().hashCode() : 0);
-        result = 31 * result + (getFarmer() != null ? getFarmer().hashCode() : 0);
+        result = 31 * result + (getSupplyDetailsId() != null ? getSupplyDetailsId().hashCode() : 0);
+        result = 31 * result + (getFarmerId() != null ? getFarmerId().hashCode() : 0);
         result = 31 * result + (getAvailableQnt() != null ? getAvailableQnt().hashCode() : 0);
         result = 31 * result + (getsDate() != null ? getsDate().hashCode() : 0);
         result = 31 * result + (getsPrice() != null ? getsPrice().hashCode() : 0);
@@ -112,8 +123,8 @@ public class Stock {
     public String toString() {
         return "Stock{" +
                 "stockId=" + stockId +
-                ", supplyDetails=" + supplyDetails +
-                ", farmer=" + farmer +
+                ", supplyDetailsId=" + supplyDetailsId +
+                ", farmerId=" + farmerId +
                 ", availableQnt=" + availableQnt +
                 ", sDate=" + sDate +
                 ", sPrice=" + sPrice +
--- src/market/dao/StockDAO.java
+++ src/market/dao/StockDAO.java
@@ -9,7 +9,7 @@ import market.bean.SupplyDetails;
  * Created by LazeL on 22.01.2017.
  */
 public interface StockDAO {
-    public void create(SupplyDetails supplyDetails);
+    public void create(Integer id);
     public Stock read(Integer id);
     public void update(Integer id, Float newAvailableQnt);
     public void delete(Integer id);
