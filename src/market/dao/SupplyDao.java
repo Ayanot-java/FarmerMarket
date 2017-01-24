ed61ba13670a8ce111fd5ae67bb6904e26c15ebb
elena-malinovsky <Myllener@yandex.ru>
2017-01-24 20:57:26 +0200

    Modified according to DB

--- src/market/dao/SupplyDao.java
+++ src/market/dao/SupplyDao.java
@@ -6,17 +6,16 @@ import market.bean.Supply;
 import java.util.ArrayList;
 import java.util.Date;
 
-/**
- * Created by Tane4ka on 18.01.2017.
- */
+
+
 public interface SupplyDao {
 
     public void create(Farmer farmer);
-    public Supply read(Farmer farmer, Date saldate);
-    public ArrayList<Supply> getAllOfFarmer(Farmer farmer);
-    public ArrayList<Supply> getAllForData(Date saldate);
-    public void update(Farmer farmer, Date saldate);
-    public void delete(Farmer farmer, Date saldate);
+    public Supply read(Farmer farmer, Date sDate);
+    public ArrayList<Supply> getAllByFarmer(Farmer farmer);
+    public ArrayList<Supply> getAllByData(Date sDate);
+    public void update(Farmer farmer, Date sDate);
+    public void delete(Farmer farmer, Date sDate);
     public ArrayList<Supply> listAll();
 
 }
