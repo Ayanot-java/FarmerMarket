eae20fd34422e776f2cc50d716993f18e27b3e84
tane4kaherdel <tanysha.gerdel@gmail.com>
2017-01-24 11:17:42 +0200

    change metods

--- src/market/dao/SupplyDao.java
+++ src/market/dao/SupplyDao.java
@@ -13,7 +13,7 @@ public interface SupplyDao {
 
     public void create(Farmer farmer);
     public Supply read(Farmer farmer, Date saldate);
-    public ArrayList<Supply> getAllOfFarmer(Farmer farmer, Date saldate);
+    public ArrayList<Supply> getAllOfFarmer(Farmer farmer);
     public ArrayList<Supply> getAllForData(Date saldate);
     public void update(Farmer farmer, Date saldate);
     public void delete(Farmer farmer, Date saldate);
