f4bef69d3172c08b49e7ba86ca70876ba1c7a3c6
Vitaly Sokolov <sokoloff06@gmail.com>
2017-01-25 10:32:57 +0200

    User, UserDAO, UserDAOImpl update

--- src/market/dao/UserDAO.java
+++ src/market/dao/UserDAO.java
@@ -1,15 +1,19 @@
 package market.dao;
 
-import market.bean.User;
-
 import java.util.ArrayList;
+
+import market.bean.User;
 
 public interface UserDAO {
 
-		public void create(User user);
-		public User read(Integer id);
-		public void delete(Integer id);
-		public void update(Integer id);
-		public ArrayList<User> listAll();
-		
+	void create(String userName, String pass) throws Exception;
+
+	User read(Integer id);
+
+	User read(String username);
+	User read(String username, String pass);
+	void delete(Integer id) throws Exception;
+	void update(Integer id);
+	ArrayList<User> listAll();
+
 }
--- src/market/dao/UserDAOImpl.java
+++ src/market/dao/UserDAOImpl.java
@@ -1,30 +1,111 @@
 package market.dao;
 
 import market.bean.User;
+import market.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Vitaly Sokolov on 22.01.2017.
  */
 public class UserDAOImpl implements UserDAO {
-
-
 	//TODO: Implement methods
+	SessionFactory factory = HibernateUtil.getSessionFactory();
 
 	@Override
-	public void create(User user) {
-
+	public void create(String username, String passwd) throws Exception {
+		if (read(username) == null) {
+			Transaction tx = null;
+			try {
+				Session session = factory.openSession();
+				tx = session.beginTransaction();
+				session.save(new User(username, passwd));
+				tx.commit();
+				session.close();
+			} catch (Exception e) {
+				if (tx != null) {
+					tx.rollback();
+					e.printStackTrace();
+				}
+			}
+		} else {
+			throw new Exception("Username is already in use. Choose another one");
+		}
 	}
 
 	@Override
 	public User read(Integer id) {
-		return null;
+		Session session = factory.openSession();
+		String hql = "from User U where U.id = :id";
+		Query<User> query = session.createQuery(hql, User.class);
+		query.setParameter("id", id);
+		List<User> results = query.list();
+		session.close();
+		if (results.isEmpty()) {
+			return null;
+		} else {
+			return results.get(0);
+		}
 	}
 
 	@Override
-	public void delete(Integer id) {
+	public User read(String username) {
+		Session session = factory.openSession();
+		String hql = "from User U where U.username = :username";
+		Query<User> query = session.createQuery(hql);
+		query.setParameter("username", username);
+		Transaction tx = session.beginTransaction();
+		List<User> results = query.list();
+		session.close();
+		if (results.isEmpty()) {
+			return null;
+		} else {
+			return results.get(0);
+		}
+	}
 
+	@Override
+	public User read(String username, String passwd) {
+		Session session = factory.openSession();
+		String hql = "from User U where U.username = :username AND U.pass = :passwd";
+		Query<User> query = session.createQuery(hql);
+		query.setParameter("username", username);
+		query.setParameter("passwd", passwd);
+		Transaction tx = session.beginTransaction();
+		List<User> results = query.list();
+		session.close();
+		if (results.isEmpty()) {
+			return null;
+		} else {
+			return results.get(0);
+		}
+	}
+
+	@Override
+	public void delete(Integer id) throws Exception {
+		User user = read(id);
+		if (user != null) {
+			Transaction tx = null;
+			try {
+				Session session = factory.openSession();
+				tx = session.beginTransaction();
+				session.delete(user);
+				tx.commit();
+				session.close();
+			} catch (Exception e) {
+				if (tx != null) {
+					tx.rollback();
+					e.printStackTrace();
+				}
+			}
+		} else {
+			throw new Exception("No such user");
+		}
 	}
 
 	@Override
