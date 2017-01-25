9a5b60558585c7dec4567dcd00680f106ffc0e30
Vitaly Sokolov <sokoloff06@gmail.com>
2017-01-25 11:12:58 +0200

    Login button to UserController

--- src/market/controller/UserController.java
+++ src/market/controller/UserController.java
@@ -18,8 +18,7 @@ import market.dao.UserDAOImpl;
  * Servlet implementation class UserController
  */
 @WebServlet("/UserController")
-public class UserController extends HttpServlet 
-{
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
 	private static final String LIST_USERS = "/listUsers.jsp";
